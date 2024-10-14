c55ee51ab5129bc17b270b28a5fbafdfcb770dc8
Mohamed Zitouni <mzitouni86@gmail.com>
2024-10-14 11:26:28 +0100

    Kept changes on manage.ledgersync.spec.ts and discarded changes on common.ts

--- apps/ledger-live-desktop/tests/specs/speculos/manage.ledgersync.spec.ts
+++ apps/ledger-live-desktop/tests/specs/speculos/manage.ledgersync.spec.ts
@@ -2,6 +2,7 @@ import { test } from "../../fixtures/common";
 import { AppInfos } from "tests/enum/AppInfos";
 import { addTmsLink } from "tests/utils/allureUtils";
 import { getDescription } from "../../utils/customJsonReporter";
+import { runCliCommand } from "tests/utils/cliUtils";
 
 const app: AppInfos = AppInfos.LS;
 
@@ -9,6 +10,7 @@ test.describe(`[${app.name}] Sync Accounts`, () => {
   test.use({
     userdata: "ledgerSync",
     speculosApp: app,
+    // cliCommand: ["ledgerKeyRingProtocol --initMemberCredentials"],
   });
 
   test(
@@ -21,11 +23,35 @@ test.describe(`[${app.name}] Sync Accounts`, () => {
     },
     async ({ app }) => {
       await addTmsLink(getDescription(test.info().annotations).split(", "));
+      const keys = await runCliCommand("ledgerKeyRingProtocol --initMemberCredentials");
+
+      console.log("keys:", keys);
+
+      const cleanResult = keys
+        .replace(/[\{\}]/g, "")
+        .replace(/'/g, "")
+        .trim();
+
+      const pairs = cleanResult.split(",").map(pair => pair.trim().split(":"));
+      const resultObject = Object.fromEntries(pairs);
+
+      const pubkey = resultObject.pubkey.trim();
+      const privatekey = resultObject.privatekey.trim();
+
+      console.log("pubkey:", pubkey);
+      console.log("privatekey:", privatekey);
+
+      const initiateTrustchain = `ledgerKeyRingProtocol --getKeyRingTree --pubKey "${pubkey}" --privateKey "${privatekey}"`;
+      console.log("initiateTrustchain:", initiateTrustchain);
+
+      const result2 = await runCliCommand(initiateTrustchain);
+
+      console.log("result2:", result2);
 
       await app.layout.goToSettings();
       await app.settings.openManageLedgerSync();
       await app.ledgerSync.expectSyncAccountsButtonExist();
-
+      /*
       await app.ledgerSync.syncAccounts();
       await app.speculos.clickNextUntilText("Make sure");
       await app.speculos.confirmOperationOnDevice("Connect with");
@@ -38,7 +64,7 @@ test.describe(`[${app.name}] Sync Accounts`, () => {
       //await app.ledgerSync.expectNbSyncedInstances(1);  //TODO: Reactivate when the issue is fixed - QAA-178
       await app.ledgerSync.destroyTrustchain();
       await app.ledgerSync.expectBackupDeletion();
-      await app.drawer.close();
+      await app.drawer.close();*/
     },
   );
 });
